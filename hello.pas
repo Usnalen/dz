2cf52d5c2d17a5da6810f11050b4f7bd231c83be
Alyona Isaeva <aleisae@sfedu.com>
2023-10-08 19:04:42 +0300

    новый файл

--- hello.pas
+++ hello.pas
@@ -1,9 +1,9 @@
 ﻿begin
-  var b:=ReadInteger('Введите время:');
-  assert((b>=0)and(b<=23));
-  case b of 
-    4..10: print('Добрый день, мир!');
-    11..16: print('Добрый вечер, мир!');
-    17..22: print('Доброй ночи, мир!');
+  var a:=ReadString('Введите язык:');
+  case a of
+    'русский':print('Привет');
+    'english':print('Hello');
+    'francais':print('Salut');
+  else print('ошибка')
   end;
 end.
